7ad211a2e08b57534cbfeeffd06bd6933c42763b
dandevri <hi@dandevri.es>
2021-01-08 16:52:25 +0100

    Move output to docs folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 const CleanCSS = require("clean-css");
+
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  return {
+    dir: {
+      output: "docs"
+    }
+  };
 };
