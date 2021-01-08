611197808d8d4a7404e0cc3f44ec976a39ab75c7
dandevri <hi@dandevri.es>
2021-01-08 16:24:53 +0100

    Add styling and basic homepage

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+const CleanCSS = require("clean-css");
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+};
